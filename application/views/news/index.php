3d31c568ab637b88e516ae66bdb7f7a9fc88431c
Piseth Sok <sokpiseth777@gmail.com>
2015-02-09 23:58:52 +0700

    [TASK] Add confirm delete news

--- application/views/news/index.php
+++ application/views/news/index.php
@@ -1,4 +1,17 @@
+<script type="text/javascript">
+    var url="<?php echo base_url();?>";
+    
+	function deleteNews(id) {
+		
+        var r=confirm("Do you want to delete this?");
+        if (r==true) {
+			window.location = url+"news/delete/"+id;
+		} else {
+			return false;
+		}
 
+	}
+</script>
 <?php echo anchor('news/create', 'New news'); ?>
 <table border="1" id="table1" >
   <tr>
@@ -21,7 +34,8 @@
 	</td>
 	<td><?php echo anchor('news/show/'.$news_item['id'] , 'show'); ?></td>
     <td><a href="<?php echo base_url()?>news/edit/<?php echo $news_item['id'] ?>">edit</a></td>
-	<td><a href="<?php echo base_url()?>news/delete/<?php echo $news_item['id'] ?>">delete</a></td>
+	<td><a href="javascript:void(0);" onclick="deleteNews(<?php echo $news_item['id'] ?>);">delete</a></td>
+	
   </tr>
 <?php endforeach ?>
 </table>
